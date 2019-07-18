75640c73f946d1fa07a1d7f3d521ecce450186d4
Dan Moffat <danmoffat@gmail.com>
2019-07-18 17:01:03 +0100

    Refactored CartIcon component to be a little cleaner - split reduce() logic out to mapStateToProps function

--- src/components/cart-icon/CartIcon.jsx
+++ src/components/cart-icon/CartIcon.jsx
@@ -4,20 +4,20 @@ import { connect } from 'react-redux';
 import { toggleCartHidden } from '../../actions';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
-const CartIcon = ({ toggleCartHidden, cartItems }) => {
+const CartIcon = ({ toggleCartHidden, itemCount }) => {
   return (
     <div onClick={toggleCartHidden} className="cart-icon">
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">
-        {cartItems.reduce((acc, item) => {
-          return acc + item.quantity;
-        }, 0)}
-      </span>
+      <span className="item-count">{itemCount}</span>
     </div>
   );
 };
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({ cartItems });
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  itemCount: cartItems.reduce((acc, item) => {
+    return acc + item.quantity;
+  }, 0)
+});
 
 export default connect(
   mapStateToProps,
